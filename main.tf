e6e2024deabb2c80b632199332c1040d78ffe8f9
Konrad Obal <konrad.obal@miquido.com>
2019-10-23 16:17:55 +0200

    feat: Upgrade to terrafrom 0.12 syntax

--- main.tf
+++ main.tf
@@ -1,46 +1,39 @@
 # Environment variables are composed into the container definition at output generation time. See outputs.tf for more information.
 locals {
   container_definition = {
-    name                   = "${var.container_name}"
-    image                  = "${var.container_image}"
+    name                   = var.container_name
+    image                  = var.container_image
     memory                 = "memory_sentinel_value"
     memoryReservation      = "memory_reservation_sentinel_value"
     cpu                    = "cpu_sentinel_value"
-    essential              = "${var.essential}"
-    entryPoint             = "${var.entrypoint}"
-    command                = "${var.command}"
-    workingDirectory       = "${var.working_directory}"
-    readonlyRootFilesystem = "${var.readonly_root_filesystem}"
-    mountPoints            = "${var.mount_points}"
-    dnsServers             = "${var.dns_servers}"
-    ulimits                = "${var.ulimits}"
-    repositoryCredentials  = "${var.repository_credentials}"
-    links                  = "${var.links}"
-    volumesFrom            = "${var.volumes_from}"
-    user                   = "${var.user}"
-    dependsOn              = "${var.container_depends_on}"
+    essential              = var.essential
+    entryPoint             = var.entrypoint
+    command                = var.command
+    workingDirectory       = var.working_directory
+    readonlyRootFilesystem = var.readonly_root_filesystem
+    mountPoints            = var.mount_points
+    dnsServers             = var.dns_servers
+    ulimits                = var.ulimits
+    repositoryCredentials  = var.repository_credentials
+    links                  = var.links
+    volumesFrom            = var.volumes_from
+    user                   = var.user
+    dependsOn              = var.container_depends_on
     stopTimeout            = "stop_timeout_sentinel_value"
-    privileged             = "${var.privileged}"
-
-    portMappings = "${var.port_mappings}"
-
-    healthCheck = "${var.healthcheck}"
-
-    firelensConfiguration = {
-      type    = "${var.firelens_type}"
-      options = "${var.firelens_options}"
-    }
-
+    privileged             = var.privileged
+    portMappings           = var.port_mappings
+    healthCheck            = length(var.healthcheck.command) > 0 ? var.healthcheck : null
+    firelensConfiguration  = var.firelens_configuration.type == null ? null : var.firelens_configuration
     logConfiguration = {
-      logDriver = "${var.log_driver}"
-      options   = "${var.log_options}"
+      logDriver = var.log_driver
+      options   = var.log_options
     }
-
     environment  = "environment_sentinel_value"
     secrets      = "secrets_sentinel_value"
     dockerLabels = "docker_labels_sentinel_value"
   }
 
-  environment = "${var.environment}"
-  secrets     = "${var.secrets}"
+  environment = var.environment
+  secrets     = var.secrets
 }
+
